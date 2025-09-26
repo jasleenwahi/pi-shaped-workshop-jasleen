959fd7349fdc4091ef1bdc6de3f7cbf5b2574a8a
jasleenwahi <jasleen.wahi@nashtechglobal.com>
2025-09-26 10:36:41 +0530

    Added assignment for day 2

---
+++ Security_Compliance_workshop-Jasleen/day2/sample-application/src/main/java/com/example/sample_application/SampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.sample_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+
+}
