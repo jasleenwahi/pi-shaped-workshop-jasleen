1383a56fb664b1598018a3c60381e3059f60d13d
jasleenwahi <jasleen.wahi@nashtechglobal.com>
2025-10-03 10:59:46 +0530

    Added trivy scan stage in the pipeline

---
+++ Security_Compliance_workshop-Jasleen/day4/sample-app/src/main/java/com/example/demo/Controller/UserController.java
@@ -0,0 +1,22 @@
+package com.example.demo.Controller;
+
+import com.example.demo.entity.Users;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/all")
+    public List<Users> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
